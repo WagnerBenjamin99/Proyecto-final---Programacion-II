16926a5c068022a97bed1d06edddd8daf9a8f1ed
Benjamin <benjaminwagner19@outlook.com>
2022-12-17 19:45:39 -0300

    creacion de menus

--- cliente.py
+++ cliente.py
@@ -1,2 +1,28 @@
+import json
+from os import system
+import requests
+from colorama import init, Fore, Back
 
+init(autoreset=True)
+
+menu_principal="""
+[1] Iniciar sesion
+[2] Continuar sin inicar sesion
+[3] Salir
+"""
+
+menu_registrado="""
+[1] Eliminar pelicula
+[2] Editar pelicula
+[3] Agregar pelicula
+[4] Cerrar sesion
+"""
+
+menu_edicion="""
+[1] Editar titulo
+[2] Editar genero
+[3] Editar fecha
+[4] Editar portada
+[5] Editar sinopsis
+"""
       
