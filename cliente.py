bf5da0d36c9a1002923835a4ee447303d3373bb0
FrancoGianotti <114113203+FrancoGianotti@users.noreply.github.com>
2022-12-17 20:40:43 -0300

    inicio de sesión

--- cliente.py
+++ cliente.py
@@ -25,4 +25,42 @@ menu_edicion="""
 [4] Editar portada
 [5] Editar sinopsis
 """
+
+
+while True:
+
+    print(Fore.GREEN + menu_principal)
+
+    opcion = int(input(Fore.MAGENTA +'Ingrese opcion: '))
+
+    if opcion == 3:
+        break
+    elif opcion == 2:
+        resp = requests.get('http://127.0.0.1:5000/')
+        print(resp)
+        
+        respuesta = resp.json()
+        for pelicula in respuesta:
+            for i, j in pelicula.items():
+                if i =='id_genero':
+                    generos = requests.get('http://127.0.0.1:5000/generos').json()
+                    for genero in generos:
+                        if j == genero['id_genero']:
+                            print(Back.WHITE + Fore.BLUE+  'GENERO', ' : ' , Fore.BLUE + genero['genero'])
+                else:       
+                    print(Back.WHITE + Fore.BLUE+  i.upper(), ' : ' , Fore.BLUE + str(j))
+            print('\n\n')
+        Back.RESET
+        
+    elif opcion == 1:
+        nombre = input(Fore.MAGENTA + 'Ingrese su nombre de usuario: ')
+        contraseña=input(Fore.MAGENTA + 'Ingrese su contraseña: ')
+        data = {'nombre_usuario':nombre,'contraseña':contraseña}
+
+        respuesta1 = requests.post('http://127.0.0.1:5000/login', json=data)
+
+        if respuesta1.status_code != 200:
+            system('cls')
+            print(Fore.RED + 'Usuario no registrado')
+            continue
       
